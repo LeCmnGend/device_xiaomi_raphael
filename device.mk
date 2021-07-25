578089135347dbd03ba28d3235d5acd064be6114
yashlearnpython <73551736+yash3056@users.noreply.github.com>
2021-07-25 20:22:24 +0530

    raphael: Add permission for Xiaomiparts
    this is being added to stop xiaomiparts being killed
    
    07-28 20:06:35.938  1454  1454 E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: main
    07-28 20:06:35.938  1454  1454 E AndroidRuntime: java.lang.IllegalStateException: Signature|privileged permissions not in privapp-permissions whitelist: {org.lineageos.settings (/system_ext/priv-app/XiaomiParts): android.permission.WRITE_SECURE_SETTINGS, org.lineageos.settings (/system_ext/priv-app/XiaomiParts): android.permission.INTERACT_ACROSS_USERS}
    
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -568,6 +568,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-parts.xml
+
 # Override heap growth limit due to high display density on device
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m
