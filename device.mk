7240d40038b2679973eb3f77e2a299ffc685e5d3
kubersharma001 <kubersharma001@gmail.com>
2021-08-15 16:47:58 +0000

    raphael: Set surface_flinger default props
    As seen on V12.5.5.0.RFKCNXM, must be set in vendor default.props.
    
    Change-Id: I66e69bd5e5b6cad05979763f4daa1fdb8113a941

--- device.mk
+++ device.mk
@@ -178,6 +178,14 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
     SystemUI
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696 \
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
