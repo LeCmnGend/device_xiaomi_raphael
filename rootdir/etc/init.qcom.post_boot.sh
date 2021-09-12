5a3bcf520543ff4d76cbac8392e2d268aa6bc5ce
Arian <arian.kulmer@web.de>
2021-09-12 19:28:38 +0200

    raphael: rootdir: Drop cpuset configurations
    Change-Id: Id4046e0a598849f81a9bcd90ee6bc3609435f5a0

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -132,14 +132,6 @@ case "$target" in
 	echo 400000000 > /proc/sys/kernel/sched_coloc_downmigrate_ns
 	echo 30 > /proc/sys/kernel/sched_min_task_util_for_colocation 
 
-	# cpuset parameters
-        echo 0-2     > /dev/cpuset/background/cpus
-        echo 0-3     > /dev/cpuset/system-background/cpus
-        echo 4-7     > /dev/cpuset/foreground/boost/cpus
-        echo 0-2,4-7 > /dev/cpuset/foreground/cpus
-        echo 0-7     > /dev/cpuset/top-app/cpus
-        echo 0-3     > /dev/cpuset/restricted/cpus
-
 	# Turn off scheduler boost at the end
 	echo 0 > /proc/sys/kernel/sched_boost
 
