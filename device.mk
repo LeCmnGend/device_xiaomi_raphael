e5d35a4b809fff85fae575a9b9df0cbb5c313db0
penglezos <panagiotisegl@gmail.com>
2022-01-19 23:10:38 +0200

    raphael: Build DRM vendor HIDL

--- device.mk
+++ device.mk
@@ -184,7 +184,12 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm@1.4-service.clearkey
+
+# Doze
+PRODUCT_PACKAGES += \
+    XiaomiCustomDoze
 
 # Fingerprint
 PRODUCT_PACKAGES += \
