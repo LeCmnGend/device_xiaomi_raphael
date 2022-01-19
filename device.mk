abfdba4ac80dbb10dfb63f54c763ed6f067828a5
penglezos <panagiotisegl@gmail.com>
2022-01-19 23:10:38 +0200

    raphael: Build DRM vendor HIDL

--- device.mk
+++ device.mk
@@ -199,7 +199,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm@1.4-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
