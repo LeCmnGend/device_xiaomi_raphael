b7deb23de8b4eed68c8d5872c3589b93494bfe8a
LeCmnGend <namhoang1235@gmail.com>
2022-09-28 18:22:47 +0700

    Bringup Superior OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_raphael.mk
+    $(LOCAL_DIR)/superior_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_raphael-userdebug \
-    aosp_raphael-eng
+    superior_raphael-user \
+    superior_raphael-userdebug \
+    superior_raphael-eng
--- device.mk
+++ device.mk
@@ -1,8 +1,24 @@
 #
-# Copyright (C) 2021 The PixelExperience Project
+# Copyright 2020-2021 The LineageOS Project
+# Copyright 2020-2022 The Superior OS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
@@ -15,7 +31,8 @@ $(call inherit-product, vendor/xiaomi/raphael/raphael-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-superior
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
